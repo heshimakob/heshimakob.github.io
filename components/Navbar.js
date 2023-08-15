0807819b75afa26d64b245d6e152d08634217673
heshima kob <heshimakob@gmail.com>
2023-08-15 14:56:53 +0200

    #

--- components/Navbar.js
+++ components/Navbar.js
@@ -40,19 +40,19 @@ function NavBar() {
            
           </div>
               {/* HAMBURGER BUTTON FOR MOBILE */}
-              <div className="md:hidden bg-black">
+              <div className="md:hidden bg-slate-900">
                 <button
                   className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
                   onClick={() => setNavbar(!navbar)}
                 >
                   {navbar ? (
-                    <Image src="/close.svg" width={30} height={30}   color='green' alt="logo" />
+                    <Image src="/close.svg" width={30} height={30}   color=' rgb(10, 47, 82)' alt="logo" />
                   ) : (
                     <Image
                       src="/hamburger-men.svg"
                       width={30}
                       height={30}
-                      color='green'
+                      color=' rgb(10, 47, 82)'
                       alt="logo"
                       className="focus:border-none active:border-none"
                     />
